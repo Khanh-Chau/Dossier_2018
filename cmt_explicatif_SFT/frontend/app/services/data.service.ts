c481b1a8fbfc6a6844d2bb88ef91e1bb1b2f8b0e
Khanh-Chau <ngkhanhchau@yahoo.com>
2018-08-30 16:38:11 +0200

    toutes les services de sft

--- cmt_explicatif_SFT/frontend/app/services/data.service.ts
+++ cmt_explicatif_SFT/frontend/app/services/data.service.ts
@@ -13,32 +13,35 @@ export class DataService {
   constructor(private _http: HttpClient) {}
 
   getZp(params?) {
-  //  retourner toutes les ZP si sans param passé
-  //  retourner une ZP spécifique en fonction du param passé 
+    //  retourner toutes les ZP si sans param passé
+    //  retourner une ZP spécifique en fonction du param passé
 
-  /* params doit se présenter sous forme objet, par ex:
+    /* params doit se présenter sous forme objet, par ex:
     params = {
       id_base_site: 5
-    }  */ 
+    }  */
 
     let myParams = new HttpParams();
     /* HttpParams est le corps d'une requête/ réponse HTTP qui représente les paramètres sérialisés
       En gros il représente les paramètres passés dans les () des méthodes. 
 
     */
-   
+
     for (let key in params) {
-      myParams = myParams.set(key, params[key])
-      
-    } 
-   /* 
+      myParams = myParams.set(key, params[key]);
+    }
+    /* 
     vu que params c' un objet, on itère sur cet objet
     La méthode set de myParams construit un nouveau corps de requête avec une nouvelle valeur pour le paramètre passé
     En gros il consruit un nouveau objet avec la clé est le paramètre qu'on veut passer.  
     On enregisre ça dans myParams.  
-   */ 
-    return this._http.get<any>(`${AppConfig.API_ENDPOINT}/suivi_flore_territoire/sites`, {params: myParams});
-  /*
+   */
+
+    return this._http.get<any>(
+      `${AppConfig.API_ENDPOINT}/suivi_flore_territoire/sites`,
+      { params: myParams }
+    );
+    /*
     La méthode get() retourne un Observable, mais vu qu'ici on va recevoir des données, TypeScript a besoin de savoir de quel type elles seront. 
     On doit donc ajouter le <any> (ça veut dire n'importe quel type) pour dire qu'on reçoit n'importe quel type 
     (utilisé quand on n'est pas certaint du type car le type any permet d'autoriser tous les types )
@@ -46,33 +49,39 @@ export class DataService {
   */
   }
 
-  getMaille(id_base_site: number, params?:any) {
+  getMaille(id_base_site: number, params?: any) {
     // retourner toutes les mailles d'une ZP pour pouvoir afficher sur la carte
     //  le params? c' pour que dans le front end, on précise id_type_nomenclature = Mailles 25*25 (et pas commune par ex)
     let myParams = new HttpParams();
-    
-    for (let key in params) {
-      myParams = myParams.set(key, params[key])
-      
-    } 
-    return this._http.get<any>(`${AppConfig.API_ENDPOINT}/gn_monitoring/siteareas/${id_base_site}`, {params: myParams});
-  }
 
+    for (let key in params) {
+      myParams = myParams.set(key, params[key]);
+    }
+    return this._http.get<any>(
+      `${AppConfig.API_ENDPOINT}/gn_monitoring/siteareas/${id_base_site}`,
+      { params: myParams }
+    );
+  }
 
   getInfoSite(id_base_site) {
     // pour savoir sur chaque site, quel est l'espèce examinée (affiche cd_nom mais pas nom_taxon )
-    return this._http.get<any>(`${AppConfig.API_ENDPOINT}/suivi_flore_territoire/site?id_base_site=${id_base_site}`);
-
+    return this._http.get<any>(
+      `${
+        AppConfig.API_ENDPOINT
+      }/suivi_flore_territoire/site?id_base_site=${id_base_site}`
+    );
   }
 
   postVisit(data: any) {
-    // poster une nouvelle visite ou éditer une visite déjà existée 
+    // poster une nouvelle visite ou éditer une visite déjà existée
     console.log(data);
     console.log("déjà post ! ");
-    
-    
-    return this._http.post<any>(`${AppConfig.API_ENDPOINT}/suivi_flore_territoire/visit`, data)  ;
-/*
+
+    return this._http.post<any>(
+      `${AppConfig.API_ENDPOINT}/suivi_flore_territoire/visit`,
+      data
+    );
+    /*
     On enregistre les données dans la base de données au endpoint suivi_flore_territoire/visit par la méthode post.
     La méthode post permet de lancer un appel POST, prend comme 1er argument l'URL visée 
     (ici ${AppConfig.API_ENDPOINT}/suivi_flore_territoire/visit),
@@ -81,62 +90,54 @@ export class DataService {
 */
   }
 
-  getVisits(params:any) {
-    //  retourne les visites en fonction du/des paramètre(s) passé(s). 
+  getVisits(params: any) {
+    //  retourne les visites en fonction du/des paramètre(s) passé(s).
     let myParams = new HttpParams();
-    
+
     for (let key in params) {
-      myParams = myParams.set(key, params[key])
-      
-    } 
-    
-    return this._http.get<any>(`${AppConfig.API_ENDPOINT}/suivi_flore_territoire/visits`, {params: myParams});
-    
-    
+      myParams = myParams.set(key, params[key]);
+    }
+
+    return this._http.get<any>(
+      `${AppConfig.API_ENDPOINT}/suivi_flore_territoire/visits`,
+      { params: myParams }
+    );
   }
-  
 
   getOneVisit(id_visit) {
     //  retourne une visite spécifique
-    return this._http.get<any>(`${AppConfig.API_ENDPOINT}/suivi_flore_territoire/visit/${id_visit}`);
-
+    return this._http.get<any>(
+      `${AppConfig.API_ENDPOINT}/suivi_flore_territoire/visit/${id_visit}`
+    );
   }
 
-  downloadData(params:any) {
-    // pour télécharger les données 
+  getOrganisme(params: any) {
+    //  récupérer l'organime
     let myParams = new HttpParams();
-    
-    for (let key in params) {
-      myParams = myParams.set(key, params[key])
-      
-    } 
-    
-    return this._http.get<any>(`${AppConfig.API_ENDPOINT}/suivi_flore_territoire/export_visit`, {params: myParams});
-  }
-  
 
-  getOrganisme(params:any) {
-    //  récupérer l'organime 
+    for (let key in params) {
+      myParams = myParams.set(key, params[key]);
+    }
+
+    return this._http.get<any>(
+      `${AppConfig.API_ENDPOINT}/suivi_flore_territoire/organisme`,
+      { params: myParams }
+    );
+  }
+
+  getCommune(id_base_site: number, params: any) {
+    // récupérer le nom de commune
     let myParams = new HttpParams();
-    
-    for (let key in params) {
-      myParams = myParams.set(key, params[key])
-      
-    } 
-    
-    return this._http.get<any>(`${AppConfig.API_ENDPOINT}/suivi_flore_territoire/organisme`, {params: myParams});
-  }
 
-  getCommune(id_base_site:number, params:any) {
-    // récupérer le nom de commune 
-    let myParams = new HttpParams();
-    
     for (let key in params) {
-      myParams = myParams.set(key, params[key])
-      
-    } 
-    
-    return this._http.get<any>(`${AppConfig.API_ENDPOINT}/suivi_flore_territoire/info_zp/${id_base_site}`, {params: myParams});
-  }
+      myParams = myParams.set(key, params[key]);
+    }
 
+    return this._http.get<any>(
+      `${
+        AppConfig.API_ENDPOINT
+      }/suivi_flore_territoire/info_zp/${id_base_site}`,
+      { params: myParams }
+    );
+  }
 }
--- cmt_explicatif_SFT/frontend/app/services/store.service.ts
+++ cmt_explicatif_SFT/frontend/app/services/store.service.ts
@@ -1,48 +1,52 @@
-import { Injectable } from '@angular/core';
-import { Layer } from 'leaflet';
-import { NgbDateParserFormatter, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { HttpParams } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import { Layer } from "leaflet";
+import { AppConfig } from "@geonature_config/app.config";
+import { ModuleConfig } from "../module.config";
 
 @Injectable()
 export class StoreService {
+  public currentLayer: Layer;
 
-public currentLayer: Layer;
+  public sftConfig = ModuleConfig;
 
-
-public myStylePresent = {
+  public myStylePresent = {
     color: "#008000",
     fill: true,
     fillOpacity: 0.2,
     weight: 3
-};
+  };
 
-public myStyleAbsent = {
+  public myStyleAbsent = {
     color: "#8B0000",
     fill: true,
     fillOpacity: 0.2,
     weight: 3
-};
+  };
 
-public presence = 0;
-public absence = 0; 
-public total = 0;
-public rest = 0; 
-public exportFormat = ['geojson', 'csv', 'shapefile'];
+  public presence = 0;
+  public absence = 0;
+  public total = 0;
+  public rest = 0;
+  public urlLoad = `${AppConfig.API_ENDPOINT}${
+    ModuleConfig.api_url
+  }/export_visit`;
+  public queryString = new HttpParams();
 
-    
-    constructor(private _modalService: NgbModal) { } 
-   
+  constructor(private _modalService: NgbModal) {}
 
-    getMailleNoVisit() {
-        // calculer les mailles ne sont pas encore visitées
-        this.rest = this.total - this.absence - this.presence;
-     }
-   
-    openIntesectionModal(content) {
-        // ouvrir un modal quand clique 
-        this._modalService.open(content);
-      }
+  getMailleNoVisit() {
+    // calculer les mailles ne sont pas encore visitées
+    this.rest = this.total - this.absence - this.presence;
+  }
 
-/* 
+  openModal(content) {
+    // ouvrir un modal quand clique
+    this._modalService.open(content);
+  }
+
+  /* 
 ces 2 méthodes je les mets ici pour que plusieurs composants puissent les utiliser. 
 */
 }
